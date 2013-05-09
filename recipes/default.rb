510c344c871486783e1ec7ca8436ae33cc042deb
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-05-09 23:53:21 +0400

    removed optipng

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@ template "/home/#{node['users']['user']}/.gemrc" do
   source "gemrc.erb"
 end
 
-%w{build-essential git-core bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev ssl-cert subversion libxslt-dev libxml2-dev imagemagick libmagickcore-dev libmagickwand-dev optipng jpegoptim}.each do |pkg|
+%w{build-essential git-core bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev ssl-cert subversion libxslt-dev libxml2-dev}.each do |pkg|
   package pkg do
     action :install
   end
